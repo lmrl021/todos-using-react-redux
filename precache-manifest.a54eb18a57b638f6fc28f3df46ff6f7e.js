ad297597b7987a31d3de1ea7d0349a4687efeb0a
lmrl021 <lmarlsaria21@gmail.com>
2020-03-31 14:03:52 +0800

    Updates

---
+++ precache-manifest.a54eb18a57b638f6fc28f3df46ff6f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e77b4a953fdf8b1c2a1c421fa894ff",
+    "url": "/todos-using-react-redux/index.html"
+  },
+  {
+    "revision": "c528e1845a147d70cba1",
+    "url": "/todos-using-react-redux/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c528e1845a147d70cba1",
+    "url": "/todos-using-react-redux/static/js/2.d23e1aee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos-using-react-redux/static/js/2.d23e1aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8bd1e1affdd21f08ca",
+    "url": "/todos-using-react-redux/static/js/main.028c6478.chunk.js"
+  },
+  {
+    "revision": "c576d89cb15c0522f02c",
+    "url": "/todos-using-react-redux/static/js/runtime-main.72dce034.js"
+  }
+]);
